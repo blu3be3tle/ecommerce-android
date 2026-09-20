4046489b65e86d7d9953829827199b0c9bc002fe
blu3be3tle <aburatinshoeb@gmail.com>
2026-09-20 17:23:34 +0600

    feat(auth): add AuthRetrofitClient

---
+++ app/src/main/java/com/example/labyshop/AuthRetrofitClient.kt
@@ -0,0 +1,25 @@
+package com.example.labyshop
+
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+
+object AuthRetrofitClient {
+    private const val BASE_URL = "https://api.auth.shajme.com"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val api: AuthAPI by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                json.asConverterFactory("application/json".toMediaType())
+            )
+            .build()
+            .create(AuthAPI::class.java)
+    }
+}
